6e248bd4319f5e846bb353fb3865937d88bf9521
pradeep-krishnaa <pradeepkrishnaa.2205108@srec.ac.in>
2025-08-27 16:18:23 +0530

    added portfolio

---
+++ Day2/Portfolio/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home';
+import { EducationComponent } from './pages/education/education';
+import { ProjectsComponent } from './pages/projects/projects';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: '**', redirectTo: '' } // fallback
+];
--- Day2/day2-project/src/app/app.ts
+++ Day2/day2-project/src/app/app.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
     
 // app component decorator
 @Component({
